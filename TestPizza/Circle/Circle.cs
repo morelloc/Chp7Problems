3274ab982b7053df299ad8406780ceb7763ab610
djsquared98 <djsquared98@yahoo.com>
2013-09-10 08:44:30 -0400

    TestPizza

---
+++ TestPizza/Circle/Circle.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Problem3
+{
+    class Circle
+    {
+        public double radius;
+        public double diameter;
+        public double area;
+    }
+        Circle round = new Circle();
+        Circle.radius = 3; 
+}
---
+++ TestPizza/TestPizza/TestPizza.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Chapter7Problems
+{
+    class Pizza
+    {
+        // Private fields
+        private toppings;
+
+
+        // Public properties
+        public string Toppings
+        {
+            get
+            {
+                return toppings;
+            }
+            set
+            {
+                toppings = value;
+            }
+        }
+
+        public int Diameter
+        {
+            get
+            {
+                return diameter;
+            }
+            set
+            {
+                diameter = value;
+            }
+        }
+
+        public double Price
+        {
+            get
+            {
+                return price;
+            }
+            set
+            {
+                price = value;
+            }
+        }
+    }
+}
