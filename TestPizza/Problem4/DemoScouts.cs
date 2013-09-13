4dba7bb22cd272fa86e7b723bcb42712a241a26a
morelloc <morellocm@gmail.com>
2013-09-13 08:42:50 -0400

    On Girlscouts

---
+++ TestPizza/Problem4/DemoScouts.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Problem5
+{
+    class GirlScout
+    {
+        private string name;
+        private double number;
+        private double dues;
+        public const string motto = ("to obey the Girl Scout law");
+
+        public GirlScout(string name, double number, double dues)
+        
+        
+
+       
+        
+  
+
+
+    }
+        
+        
+}
